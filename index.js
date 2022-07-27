0bbcd14dd73749d86ca36d7015088b4b4b7b6e50
Your Name <you@example.com>
2022-07-27 15:39:02 +0600

    tools api and .env added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,10 +10,37 @@ app.use(cors());
 app.use(express.json());
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jj6dq.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+  try {
+    // to connect to database tools-portal/tools
+    await client.connect();
+    const serviceCollection = client.db('bdtools_portal').collection('tools');
+
+    // to get all tools / TOOLS API
+
+    app.get('/tools', async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const tools = await cursor.toArray();
+      res.send(tools);
+    });
+
+  }
+  finally {
+
+  }
+}
+
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
-  res.send('Hello Doctor Portal!')
+  res.send('Hello BDTools!')
 })
 
 app.listen(port, () => {
-  console.log(`Doctors app listening on port ${port}`)
+  console.log(`BDTools app listening on port ${port}`)
 })
