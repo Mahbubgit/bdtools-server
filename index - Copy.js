89286499ccf08881befb4f0d2643f2e41acc0fc5
Your Name <you@example.com>
2022-07-30 13:04:57 +0600

    add review and my orders api added

--- index - Copy.js
+++ index - Copy.js
@@ -5,7 +5,6 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 const jwt = require('jsonwebtoken');
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 // middleware
 app.use(cors());
@@ -23,7 +22,7 @@ function verifyJWT(req, res, next) {
     if (err) {
       return res.status(403).send({ message: 'Forbidden Access' });
     }
-    // console.log('decoded', decoded);
+    console.log('decoded', decoded);
     req.decoded = decoded;
     next();
   });
@@ -44,21 +43,7 @@ async function run() {
     const userCollection = client.db('bdtools_portal').collection('users');
 
 
-    // To verify whether an user is admin or not
-
-    const verifyAdmin = async (req, res, next) => {
-      const requester = req.decoded.email;
-      const requesterAccount = await userCollection.findOne({ email: requester });
-      if (requesterAccount.role === 'admin') {
-        next();
-      }
-      else {
-        res.status(403).send({ message: 'Forbidden' });
-      }
-    }
-
     // Authentication
-
     app.post('/login', async (req, res) => {
       const user = req.body;
       const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
@@ -66,42 +51,14 @@ async function run() {
       });
       res.send({ accessToken });
     });
-
-    // To create payment intent
-
-    app.post('/create-payment-intent', verifyJWT, async (req, res) => {
-      const order = req.body;
-      const price = order.price;
-      const amount = price * 100;
-      const paymentIntent = await stripe.paymentIntents.create({
-        amount: amount,
-        currency: 'usd',
-        payment_method_types: ['card']
-      });
-      res.send({ clientSecret: paymentIntent.client_secret })
-    });
-
-
     /*****************************USER API***************************************** */
 
-    // to view all user
+    // to Get all user
 
-    app.get('/user', verifyJWT, async (req, res) => {
-      const users = await userCollection.find().toArray();
-      res.send(users);
-    });
-
-    // To show all user with role (make an admin)
-    app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
-      const email = req.params.email;
-
-      const filter = { email: email };
-      const updateDoc = {
-        $set: { role: 'admin' },
-      };
-      const result = await userCollection.updateOne(filter, updateDoc);
-      res.send(result);
-    });
+    //   app.get('/user', verifyJWT, async (req, res) => {
+    //     const users = await userCollection.find().toArray();
+    //     res.send(users);
+    // });
 
     // To add an user to database
     app.put('/user/:email', async (req, res) => {
@@ -118,32 +75,8 @@ async function run() {
       res.send({ result, token });
     });
 
-    // To update an user profile to database
-
-    app.put('/user/profile/:email', async (req, res) => {
-      const email = req.params.email;
-      const user = req.body;
-      const filter = { email: email };
-      const options = { upsert: true };
-
-      const updateDoc = {
-        $set: user,
-      };
-      const result = await userCollection.updateOne(filter, updateDoc, options);
-      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' });
-      res.send({ result, token });
-    });
-
-    // To pick an user who have admin role
-    app.get('/admin/:email', async (req, res) => {
-      const email = req.params.email;
-      const user = await userCollection.findOne({ email: email });
-      const isAdmin = user.role === 'admin';
-      res.send({ admin: isAdmin });
-    })
 
     /********************TOOLS API*************************** */
-
     // to get all tools / TOOLS API
 
     app.get('/tools', async (req, res) => {
@@ -162,13 +95,6 @@ async function run() {
       res.send(tool);
     });
 
-    // To add a tool
-    // app.post('/tool', verifyJWT, verifyAdmin, async (req, res) => {
-    //   const tool = req.body;
-    //   const result = await toolsCollection.insertOne(tool);
-    //   res.send(result);
-    // });
-
     /******************************Order API************************************ */
 
     // to post an order/ PURCHASE/ORDER API
@@ -177,7 +103,7 @@ async function run() {
       const order = req.body;
       const result = await orderCollection.insertOne(order)
       res.send(result);
-    });
+    })
 
     // For Cancel/delete an Order / DELETE ORDER API
 
@@ -196,25 +122,27 @@ async function run() {
     app.get('/order', verifyJWT, async (req, res) => {
       const email = req.query.email;
       const decodedEmail = req.decoded.email;
+      // console.log('email', email, ', decodedEmail', decodedEmail);
       if (email === decodedEmail) {
-        const query = { email: email };
-        const result = await orderCollection.find(query).toArray();
-        res.send(result);
+          const query = { email: email };
+          const result = await orderCollection.find(query).toArray();
+          res.send(result);
       }
       else {
-        return res.status(403).send({ message: 'Forbidden Access' });
+          return res.status(403).send({ message: 'Forbidden Access' });
       }
 
     });
+
 
     // to payment of an Order / PAYMENT ORDER API
 
-    app.get('/order/:id', verifyJWT, async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const paymentOrder = await orderCollection.findOne(query);
-      res.send(paymentOrder);
-    });
+    // app.get('/order/:id', verifyJWT, async (req, res) => {
+    //     const id = req.params.id;
+    //     const query = { _id: ObjectId(id) };
+    //     const purchaseOrder = await orderCollection.findOne(query);
+    //     res.send(purchaseOrder);
+    // });
 
     // to get all orders
     // app.get('/order', async (req, res) => {
@@ -232,15 +160,6 @@ async function run() {
       const cursor = reviewsCollection.find(query);
       const reviews = await cursor.toArray();
       res.send(reviews);
-    });
-
-
-    // to post a review/ REVIEW API
-
-    app.post('/review', async (req, res) => {
-      const review = req.body;
-      const result = await reviewsCollection.insertOne(review)
-      res.send(result);
     });
 
   }
